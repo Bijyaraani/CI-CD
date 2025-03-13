91fb84b1ff07fa3c01124185ec2db556f8398e3f
Anshika Singh <100311615+Bijyaraani@users.noreply.github.com>
2025-03-13 18:44:16 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,11 +1,8 @@
-#test app
-#this is for testing purpose
-def(a,b):
-return a+b
+# app.py
+# This is a test commit
+def add(a, b):
+    return a + b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
-
-
-  
